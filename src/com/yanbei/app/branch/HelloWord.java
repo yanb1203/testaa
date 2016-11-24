382adf3d73ec8a828889ee5e812fb8c423c479f3
anne_yan <anne_yan@infosys.com>
2016-11-24 11:22:18 +0800

    branch add

---
+++ src/com/yanbei/app/branch/HelloWord.java
@@ -0,0 +1,14 @@
+package com.yanbei.app.branch;
+
+public class HelloWord {
+
+    public HelloWord() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
