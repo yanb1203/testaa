4c3516c01d648b0d18264c4359b36f2b75f716d8
anne_yan <anne_yan@infosys.com>
2016-11-24 18:04:49 +0800

    ttest anne

--- src/com/yanbei/app/branch/HelloWord.java
+++ src/com/yanbei/app/branch/HelloWord.java
@@ -4,8 +4,13 @@ package com.yanbei.app.branch;
 public class HelloWord {
 
     public HelloWord() {
+        //anne
         //sssss
         //ooo
+        ///anne
+        // 11
+        // aaaa
+        // TODO Auto-generated constructor stub
     }
 
     public static void main(String[] args) {
